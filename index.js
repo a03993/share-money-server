b6f0a2cc4107b11e75993adb5e3190cec4951de8
Tina <98255708+a03993@users.noreply.github.com>
2025-04-07 14:20:05 +0800

    fix: update server port from 5000 to 5001

--- index.js
+++ index.js
@@ -23,5 +23,5 @@ mongoose
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log(err));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
